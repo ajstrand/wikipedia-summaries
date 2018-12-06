84cdabf06952857ca7b52be3f40ee88acb982e85
Alex <ajstrand8@gmail.com>
2018-12-05 20:08:21 -0500

    working on deploying to heroku

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import "log"
+import "github.com/levigross/grequests"
+import "net/http"
+
+type Result struct {
+    Text   string        `json:"extract"`
+}
+
+func getSummary(w http.ResponseWriter, r *http.Request) {
+
+	message := r.URL.Query().Get("data")	
+
+	var url = "https://en.wikipedia.org/api/rest_v1/page/summary/" + message;
+
+	resp, err := grequests.Get(url, nil)
+// You can modify the request by passing an optional RequestOptions struct
+
+if err != nil {
+	log.Fatalln("Unable to make request: ", err)
+}
+var result Result
+resp.JSON(&result)
+var data = []byte(result.Text)
+	w.Write(data)
+  }
+  func main() {
+	http.HandleFunc("/api/getSummary", getSummary)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+	  panic(err)
+	}
+}
