08d632f97bf5762423197af17f2b13d6b8e18083
Alex <ajstrand8@gmail.com>
2018-12-09 18:36:42 -0500

    update css colors

--- src/Summary.js
+++ src/Summary.js
@@ -20,7 +20,7 @@ const styles = (theme) => ({
             width:"30%",
         },
         backgroundColor: 'palevioletred',
-        color:'papayawhip!important'
+        color:'papayawhip'
     },
     input: {
         margin:'1em',
